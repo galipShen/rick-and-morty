f2a97f68871226164ba09d83766821107c0e1570
Galip Sen <galipshen@gmail.com>
2024-03-16 14:58:24 +0300

    redux configured

--- App.js
+++ App.js
@@ -3,12 +3,29 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Router from "./src/screens/Router/Router";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Router />
-      <StatusBar translucent={true} />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router />
+        <StatusBar translucent={true} />
+      </NavigationContainer>
+    </Provider>
   );
 }
+/**
+ * 
+ * 
+ * import { Provider } from 'react-redux';
+import { store } from './src/store/store';
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Text>Hello World!</Text>
+    </Provider>
+  );
+}
+ */
--- src/screens/Router/HomeScreen.js
+++ src/screens/Router/HomeScreen.js
@@ -1,16 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { Counter } from "../../components/Counter";
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Episode")}>
+    <View style={styles.container}>
+      {/* <Text>HomeScreen</Text>
+      <TouchableOpacity
+        style={{ flex: 1 }}
+        onPress={() => navigation.navigate("Episode")}
+      >
         <Text>BASSS</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
+      <Counter />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "green",
+  },
+});
